3e933563a129a6063e6da076d373fc795b131dbb
Raul Huertas <rax20037@gmail.com>
2026-08-06 00:53:50 -0500

    reflow working

--- shared/src/commonMain/kotlin/com/rhuertas/flowlayout/App.kt
+++ shared/src/commonMain/kotlin/com/rhuertas/flowlayout/App.kt
@@ -7,16 +7,25 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowColumn
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.painterResource
 
 import flowlayout.shared.generated.resources.Res
@@ -30,20 +39,43 @@ fun App() {
         FlowColumn(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
+                .fillMaxSize()
                 .safeContentPadding()
-                .fillMaxSize(),
+                //.fillMaxSize(1.0f)
+            ,
             horizontalArrangement = Arrangement.Center,
-            verticalArrangement = Arrangement.Bottom,
-            itemHorizontalAlignment = Alignment.CenterHorizontally
+            verticalArrangement = Arrangement.Center,
+            itemHorizontalAlignment = Alignment.CenterHorizontally,
+            //maxItemsInEachColumn = 2,
         ) {
-            Button(
-                onClick = {
-                    showContent = !showContent
-                },
-            ) {
-                Text("Click me!")
+            FlowRow (
+                //itemVerticalAlignment = Alignment.CenterVertically,
+                maxItemsInEachRow = 3,
+                //modifier = Modifier.weight(0.2f),
+            ){
+                for (i in 0..<5) {
+                    Text(
+                        text = "Item $i",
+                        textAlign = TextAlign.Center,
+                    )
+                }
+                Button(
+                    onClick = {
+                        showContent = !showContent
+                    },
+                ) {
+                    Text("Click me!")
+                }
             }
-            Image(painterResource(Res.drawable.compose_multiplatform), null)
+            Image(
+                painterResource(Res.drawable.compose_multiplatform),
+                contentDescription = "mainview",
+                //Modifier.weight(1.0f, fill = true),
+                //modifier = Modifier.fillMaxSize()
+                //.weight(1.0f)
+                //modifier = Modifier.height(900.dp),
+                //contentScale = ContentScale.Fit,
+            )
         }
     }
 }
