bad68166013edcc721f8ac6837681527b77cb64c
Raul Huertas <rax20037@gmail.com>
2026-08-05 19:41:46 -0500

    basic flow column tested

--- shared/src/commonMain/kotlin/com/rhuertas/flowlayout/App.kt
+++ shared/src/commonMain/kotlin/com/rhuertas/flowlayout/App.kt
@@ -3,7 +3,10 @@ package com.rhuertas.flowlayout
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowColumn
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
@@ -23,27 +26,24 @@ import flowlayout.shared.generated.resources.compose_multiplatform
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
+        var showContent by remember { mutableStateOf(true) }
+        FlowColumn(
             modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
                 .safeContentPadding()
                 .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            horizontalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Bottom,
+            itemHorizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Button(onClick = { showContent = !showContent }) {
+            Button(
+                onClick = {
+                    showContent = !showContent
+                },
+            ) {
                 Text("Click me!")
             }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+            Image(painterResource(Res.drawable.compose_multiplatform), null)
         }
     }
 }
